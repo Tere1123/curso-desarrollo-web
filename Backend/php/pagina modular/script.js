f765aadf824e52782ea8b818551638b49328b4d5
Tere1123 <mariatere1123@hotmail.com>
2023-03-02 13:46:53 +0100

    pag

--- Backend/php/pagina modular/script.js
+++ Backend/php/pagina modular/script.js
@@ -1,8 +1,39 @@
 $(document).ready(function () {
 
-$('#slide').click(function () { 
-    $('.cont-tres').slideToggle();
-    
+  let contador = 0;
+  let pos = 'box-btn';
+
+  $('#btnSlide').click(function () {
+
+    if (contador < 1 || pos == 'box-btn') {
+      $('.box-btn').slideToggle(300, function () {
+        $('.box-form').slideToggle(300);
+      });
+      contador++;
+      pos = 'box-form';
+    } else {
+      $('.box-form').slideToggle(300, function () {
+        $('.box-btn').slideToggle(300);
+      });
+      contador--;
+      pos = 'box-btn';
+    }
+
+    // console.log('funciona');
+    // $('.box-btn').slideToggle();
+    // console.log('funciona');
   });
+
+  // $(function () {
+  //   $('.banner').unslider({
+  //     speed: 500,
+  //     delay: 3000,
+  //     complete: function () { },
+  //     keys: true,
+  //     dots: true,
+  //     fluid: false
+  //   });
+  //   console.log('funciona');
+  // });
 
 });
