f83b12be7280ceee9807da5e643d9f33a7fad611
Tere1123 <mariatere1123@hotmail.com>
2023-03-16 13:44:58 +0100

    bucles

--- Backend/bucles/index.php
+++ Backend/bucles/index.php
@@ -191,25 +191,25 @@ td.negro {
 
 </style>";
 // creamos las variables
-$max = 8;//hasta donde quiero que llegue
+$max = 8; //hasta donde quiero que llegue
 
 echo  " <table>"; //abrimos la tabla
 
 //creamos el primer contador hasta el 8
 for ($fila = 1; $fila <= $max; $fila++) {
 
-    echo  "<tr>";//abrimos la primera fila
+    echo  "<tr>"; //abrimos la primera fila
 
-    for ($celda = 1; $celda <= $max; $celda++) {// segundo contador de la celda aumentara despues de recorrer 1 al 8
+    for ($celda = 1; $celda <= $max; $celda++) { // segundo contador de la celda aumentara despues de recorrer 1 al 8
 
         echo '<td>'; //se abren las celda
         echo "$fila * $celda= " . $fila * $celda;
-        echo '</td>';//se cierran las celda
+        echo '</td>'; //se cierran las celda
     }
-    echo  "</tr>";//se cierra la fila
+    echo  "</tr>"; //se cierra la fila
 }
 
-echo "</table>";// se cierra la tabla
+echo "</table>"; // se cierra la tabla
 
 echo '<h3>9. Crear un programa de PHP que imprima un tablero de ajedrez
 // Pista: Usar una tabla con 270px de ancho y 30px como medida para las celdas </h3>';
@@ -217,22 +217,22 @@ echo '<h3>9. Crear un programa de PHP que imprima un tablero de ajedrez
 
 //creamos variables
 
-$white= "<td class='blanco'></td>" ;
+$white = "<td class='blanco'></td>";
 $negro = "<td class='negro'></td>";
 
 echo  " <table class = 'tablero'"; // creamo la tabla
 
 // imprimimos el primer bucle
-for ($t=1; $t <=4 ; $t++) {
+for ($t = 1; $t <= 4; $t++) {
     echo "<tr>";
     //imprimimos la primra fila,esta se repite dos y dos
-    for ($i=1; $i <=4 ; $i++) { 
+    for ($i = 1; $i <= 4; $i++) {
         echo "$negro";
         echo "$white";
     }
     echo "</tr>";
     //imprimimos la segunda fila con los colores inveridos y vuelve al inicio para repetir la secuencia 4 veces
-    for ($i=1; $i <=4 ; $i++) { 
+    for ($i = 1; $i <= 4; $i++) {
         echo "$white";
         echo "$negro";
     }
@@ -244,7 +244,7 @@ echo "</table>";
 echo '<h3> 10. Crear un script que imprima la siguiente tabla </h3>';
 
 // creamos las variables
-$max = 10;//hasta donde quiero que llegue
+$max = 10; //hasta donde quiero que llegue
 
 echo  " <table>"; //abrimos la tabla
 
@@ -252,70 +252,65 @@ echo  " <table>"; //abrimos la tabla
 
 for ($fila = 1; $fila <= $max; $fila++) {
 
-    echo  "<tr>";//abrimos la primera fila
+    echo  "<tr>"; //abrimos la primera fila
 
-    for ($celda = 1; $celda <= $max; $celda++) {// segundo contador de la celda aumentara despues de recorrer 1 al 8
+    for ($celda = 1; $celda <= $max; $celda++) { // segundo contador de la celda aumentara despues de recorrer 1 al 8
 
         echo '<td>'; //se abren las celda
         echo $fila * $celda;
-        echo '</td>';//se cierran las celda
+        echo '</td>'; //se cierran las celda
     }
-    echo  "</tr>";//se cierra la fila
+    echo  "</tr>"; //se cierra la fila
 }
 
-echo "</table>";// se cierra la tabla
+echo "</table>"; // se cierra la tabla
 
 
 echo '<h3> 11. Crear un programa en php que integre  los numero de 1 al 50.Al imprimirlos los multiplos de 3 se sustituiran  por fizz  los multiplos de 5 por buzz y
 los que sean mulpiplos de los dos por fizBuzz </h3>';
 
 // dos formas de hacerlo con los ifelse y switch
-for ($i=1; $i <=50 ; $i++) { 
+for ($i = 1; $i <= 50; $i++) {
 
     if ($i % 3 == 0 && $i % 5 == 0) {
         echo 'fizzbuz-';
-   
-    }elseif ($i % 3 == 0) {
+    } elseif ($i % 3 == 0) {
 
-       echo 'fizz-';
-
-     }elseif ($i % 5 == 0) {
+        echo 'fizz-';
+    } elseif ($i % 5 == 0) {
 
         echo 'buzz-';
     } else {
-     echo "$i-";
+        echo "$i-";
     }
-
 }
 
 
-echo'<hr>';
+echo '<hr>';
 
-for ($i=1; $i <=50 ; $i++) {
+for ($i = 1; $i <= 50; $i++) {
 
-switch ($i) {
+    switch ($i) {
 
-    case $i % 3 == 0 && $i % 5 == 0;
-    echo 'fizzbuzz- ';
-    break;
+        case $i % 3 == 0 && $i % 5 == 0;
+            echo 'fizzbuzz- ';
+            break;
 
-    case $i % 3 == 0;
-    echo 'fizz- ';
-    break;
-    
-    case $i % 5 == 0;
-     echo 'buzz-';
-     break;
+        case $i % 3 == 0;
+            echo 'fizz- ';
+            break;
 
-     default:
-     echo "$i - ";
-     break;
+        case $i % 5 == 0;
+            echo 'buzz-';
+            break;
 
-} 
-
+        default:
+            echo "$i - ";
+            break;
+    }
 }
 
-echo'<hr>';
+echo '<hr>';
 
 echo '<h3> 12-crear un triangulo de floyd </h3>';
 
@@ -325,33 +320,31 @@ $nfila = 12;
 $contador = 1;
 $espacio = "&nbsp;&nbsp;";
 
-for ($i=0; $i <=$nfila ; $i++) { 
+for ($i = 0; $i <= $nfila; $i++) {
 
-    for ($c=1; $c <= $i; $c++) { 
+    for ($c = 1; $c <= $i; $c++) {
         echo  $espacio;
         echo  $contador++;
-      
     }
-    echo'<br>';
+    echo '<br>';
 }
 
 
-echo'<hr> por el profe';
+echo '<hr> por el profe';
 
 
 $nfila = 12;
 $contador = 1;
 
-for ($i=1; $i <=$nfila ; $i++) { 
- 
-    for ($b=1; $b <=$i; $b++ ) {
+for ($i = 1; $i <= $nfila; $i++) {
 
-       echo $contador;
-       echo  $espacio;
+    for ($b = 1; $b <= $i; $b++) {
+
+        echo $contador;
+        echo  $espacio;
         $contador++;
-       
     }
-    echo'<br>';
+    echo '<br>';
 }
 
 echo '<h3> 13-escribir un bucle de php que imprima una letra del abecedario  
@@ -365,11 +358,30 @@ echo '<h3> 13-escribir un bucle de php que imprima una letra del abecedario
 // *       * </h3>';
 
 
+
+$espacios = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+echo "<pre>";
+for ($i = 1; $i <= 8; $i++) {
+
+    if ($i != 1) {
+        echo "* ";
+    } else echo "&nbsp;&nbsp;";
+
+    if ($i == 1 || $i == 4) {
+        echo "* * *";
+    } else echo $espacios;
+
+    if ($i != 1) echo " *";
+    echo "<br>";
+}
+echo "</pre>";
+
+
 // for ($i=0; $i < ; $i++) { 
-   
+
 // }
 
-echo '<h3> 13- 14. Escribir un bucle de PHP que sea capaz de imprimir este patrón:
+echo '<h3> 14. Escribir un bucle de PHP que sea capaz de imprimir este patrón:
 
 // 1 5 9 <br>
 // 2 6 10 <br>
@@ -377,19 +389,52 @@ echo '<h3> 13- 14. Escribir un bucle de PHP que sea capaz de imprimir este patr�
 // 4 8 12 <br></h3>';
 
 
+$n = 4;
 
-
-$nfila1 = 4;
-$conta = 1;
-// $espacio = "&nbsp;&nbsp;";
-
-for ($i=1; $i <=$nfila1 ; $i++) { 
-//    echo $i;
-     for ($b=1; $b <= $nfila1 ; $c++) { 
-
-        $conta;
-        
+for ($i = 1; $i <= $n; $i++) {
+    echo $i . " ";
+    for ($b = 1; $b < $n; $b++) {
+        echo ($i + $n * $b) . " ";
     }
-    echo $$conta;
-    echo'<br>';
+    echo '<br>';
+}
+
+// $nfila1 = 4;
+// $n=3;
+// // $espacio = "&nbsp;&nbsp;";
+
+// for ($i = 1; $i <= $nfila1; $i++) {
+
+//     for ($b = $i; $b <= $n; $b++) {
+   
+//     }
+ 
+  
+// }
+
+echo  " </table>";
+
+echo '<h3>  15. Escribir un bucle de PHP que sea capaz de imprimir este patrón:
+
+$n = 5;
+// 1
+// 121
+// 12321
+// 1234321
+// 12345432</h3>';
+
+
+echo '<br>';
+
+$n = 6;
+
+for ($i = 1; $i <= $n; $i++) {
+
+    for ($j = 1; $j <= $i; $j++) {
+        echo $j . " ";
+    }
+    for ($j = $i - 1; $j >= 1; $j--) {
+        echo $j . " ";
+    }
+    echo '<br>';
 }
